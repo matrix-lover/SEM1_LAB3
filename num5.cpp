63e34134b28bb2d9d9a3d06fcddf4618eb69869b
Shamil Krut <gmific@yandex.ru>
2024-10-18 22:55:37 -0700

    Create num5.cpp

---
+++ num5.cpp
@@ -0,0 +1,13 @@
+//exercise num 5
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string city;
+    cin >> city;
+    cout << '\n';
+    cout << size(city); // количество городов
+    return 0;
+}
