95cea461eaae8ae0f81a73206ac1bbf324093819
Shamil Krut <gmific@yandex.ru>
2024-10-20 07:28:30 -0700

    Update num9.cpp

--- num9.cpp
+++ num9.cpp
@@ -7,6 +7,11 @@ using namespace std;
 int main() {
     string verb;
     cin >> verb;
+    int utf = 1;
+    string condi;
+    cout << "Слово на русском языке?(да|нет)" << endl;
+    cin >> condi;
+    if (condi != "нет"){utf = 2;}
     string star = "*";
     string stars = "";
     int count = 0; //счетчик
@@ -16,13 +21,16 @@ int main() {
         count += 1;         //аналогично .size
     }
     
+    count = count / utf;
+    
     for (int i = 0; i < count; ++i){
         stars.insert(i, star);
     }
-    
+    count = count * 1.5;
     verb.insert(0, stars);
     verb.insert(count*2, stars);
     
     cout << verb << endl;
     return 0;
 }
+
