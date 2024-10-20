fae9e79ad781c8980a71639230327e41ff35b578
Shamil Krut <gmific@yandex.ru>
2024-10-20 03:50:49 -0700

    Update num6.cpp

--- num6.cpp
+++ num6.cpp
@@ -9,9 +9,19 @@ int main() {
     string sec_name2;
     cin >> sec_name1;
     cin >> sec_name2;
+    int count1 = 0;
+    int count2 = 0;
     cout << '\n';
-    if (size(sec_name1) > size(sec_name2)){
-        cout << sec_name1;
-    } else {cout << sec_name2;}
+    while (sec_name1[count1] != 0){ // аналогично .size()
+        count1 += 1;
+    }
+    while (sec_name2[count2] != 0){ // аналогично .size()
+        count2 += 1;
+    }
+    if (count1 > count2){ cout << "Название первого города длиннее, чем у второго" << endl;}
+    else if (count1 < count2){cout << "Название второго города длиннее, чем у первого" << endl;}
+    else {cout << "Название городов одной длины" << endl;}
+    cout << count1;
+    cout << count2;
     return 0;
 }
